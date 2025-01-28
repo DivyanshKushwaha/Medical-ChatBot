e2c8e4c55196075a9fe1fef3fa3f9e2bb40b8761
Divyansh Kushwaha <jatinkushwaha309@gmail.com>
2025-01-28 19:53:09 +0530

    helper files added

---
+++ src/helper.py
@@ -0,0 +1,36 @@
+from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+import os 
+
+#Extract data from the PDF
+def load_pdf(data):
+    loader = DirectoryLoader(data,
+                    glob="*.pdf",
+                    loader_cls=PyPDFLoader)
+    
+    documents = loader.load()
+
+    return documents
+
+
+
+#Create text chunks
+def text_split(data_extracted):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=20)
+    text_chunks = text_splitter.split_documents(data_extracted)
+    return text_chunks
+
+
+
+#download embedding model
+def download_embeddings():
+    # Define the path where the embeddings should be saved
+    cache_dir = os.path.join(os.getcwd(), "modelEmbedd")
+    
+    # Initialize embeddings with the specified cache directory
+    embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2",
+        cache_folder=cache_dir
+    )
+    return embeddings
